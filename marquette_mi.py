c8d3e343869476ddd2a6563af9954b97b25d2eba
Paul R. Carlson <carlsonpar@gmail.com>
2020-11-12 10:54:09 -0500

    initial attempt with tabula

--- marquette_mi.py
+++ marquette_mi.py
@@ -1,3 +1,26 @@
 import tabula
-import pandas as PD
+import pandas as pd
+import PyPDF2
 
+pd.set_option('display.max_colwidth',None)
+pd.set_option('display.max_columns',None)
+pd.set_option('display.max_rows',None)
+result = []
+#top=.70*72
+#left = 3.05*72
+#bottom = 1.71*72
+# #right = 8.21*72
+
+
+
+
+data = tabula.read_pdf('marquette_mi_result.pdf',guess=True,lattice=False,stream=True,pages=('3'))
+for x in data:
+    df = x
+    df.dropna(axis='rows',how='all',inplace=True)
+    df.dropna(axis='columns',how='all',inplace=True)
+    df = df.reset_index(drop=True)
+    print('-----------')
+    print(df)
+    result.append(df)
+    pd.concat(result).to_csv('a.csv')
