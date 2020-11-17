3713324f2d539e20dd1c53b83463240cbc78e669
Paul R. Carlson <carlsonpar@gmail.com>
2020-11-17 11:32:20 -0500

    scrapping now mostly works with races that are single or two pages in length.  Output includes the actual race.

--- marquette_mi.py
+++ marquette_mi.py
@@ -5,14 +5,14 @@ import pdfplumber
 
 
 
-pd.set_option('display.max_colwidth',None)
 pd.set_option('display.max_columns',None)
 pd.set_option('display.max_rows',None)
 page = 1
 result = []
+row_drop=[]
+race=''
 continuation = False
-race =''
-columns = ['Precinct','Times Cast','Registered Voters','A','B','C','D','E','F','G','H','I','J','K','L','M']
+
 def header_split(page):
     with pdfplumber.open(r'marquette_mi_result.pdf') as pdf: 
         page = pdf.pages[page]
@@ -24,59 +24,66 @@ def header_split(page):
     #     if 'Voters' in x:
     #         print(x.split('  ')[6:])
 
-
-# with pdfplumber.open(r'marquette_mi_result.pdf') as pdf: 
-#         page = pdf.pages[6]
-#         text = page.extract_text()
-#         # print(text[3])
-# lines = text.split('\n')
-# print(lines[3])
-# for x in lines:
-#     if 'Voters' in x:
-#         print(x.split('  ')[6:])
-
-
-#found a way to extract column headers that are vertical
-
-def drop_empty(dataframe):
+#replacement of NaN values with corresponding data.
+def removal(dataframe):
+    row_drop=[]
+    for index, row in dataframe.iterrows():
+                if pd.isna(row[0]):
+                    for label, content in row.items():
+                        if pd.isna(content) == False:
+                            row_drop.append(index)
+                            try:
+                                dataframe.loc[[index+1],[label]] = content
+                            except:
+                                pass
+    dataframe.drop(dataframe.index[row_drop],inplace=True)
+    dataframe.dropna(axis='columns',how='all',inplace=True)
     dataframe.dropna(axis='rows',how='all',inplace=True)
-    df.dropna(axis='columns',how='all',inplace=True)
+    dataframe = dataframe.reset_index(drop=True)
+    row_drop=[]
+    return(dataframe)
 
-data = tabula.read_pdf('marquette_mi_result.pdf',multiple_tables=True,lattice=True,stream=True,pages=('1-20'))
+
+data = tabula.read_pdf('marquette_mi_result.pdf',multiple_tables=True,lattice=True,stream=True,pages=('9,10'))
 for x in data:
-    # print('for')
-    # continuation = False
     df = x
-    # print('-----------')
-    # print(df)
     df.dropna(axis='rows',how='all',inplace=True)
     df.dropna(axis='columns',how='all',inplace=True)
     df = df.reset_index(drop=True)
     if not df.empty:
         if continuation == False:
             for index, row in df.iterrows():
-                if df.index.isin([index]).any() and isinstance(df.iloc[index,0],str):
-                    if df.iloc[index,0] == 'County':
-                        result=[]
-                        continuation = True
-                        race = header_split(page-1)
-                        df.drop(df.index[0:index+2],inplace=True)
-                        df.dropna(axis='columns',how='all',inplace=True)
-                        # df.columns = columns
-                        df = df.reset_index(drop=True)
-                        print(df.count(axis='columns'))
-                        result.append(df)
+                if df.index.isin([index]).any() and str(df.iloc[index,0]) == 'County':
+                    result=[]
+                    continuation = True
+                    race = header_split(page-1) 
+                    df.drop(df.index[0:index+2],inplace=True)
+                    df.dropna(axis='rows',how='all',inplace=True)
+                    df.dropna(axis='columns',how='all',inplace=True)
+                    
+                    df = df.reset_index(drop=True)
+            df = removal(df)
+            for x in range(len(df.columns)):
+                df.rename({df.columns[x]:x},axis=1,inplace=True)
+            result.append(df)
+
         else:
-            # df.drop(df.columns[3],axis=1,inplace=True)
-            # df.columns = columns
-            # print(df)
+
+
+            df = removal(df)
+            for x in range(len(df.columns)):
+                df.rename({df.columns[x]:x},axis=1,inplace=True)
+
             result.append(df)
             continuation = False
+        #test stuff
+        try:
+            final = pd.concat(result)
+            final = final.reset_index(drop=True)
+        except:
+            final = df
 
-        page = page+1
         race.replace('  ','_')
-        # print('-------------------')
-        # print(race)
-        save = race + '.csv'
-        print(save)
-        pd.concat(result).to_csv(save)
+        save = race+'.csv'
+        final.to_csv(save)
+        page = page+1
