ab27283537ec5bf23b417eb013641b209938ea51
Paul R. Carlson <carlsonpar@gmail.com>
2020-12-28 20:48:58 -0500

    updated code to run multiple pages.  Correctly parses the pages, except for pg. 7.  Also adding a few extra null values on page 2.

--- Midland/midland.py
+++ Midland/midland.py
@@ -13,14 +13,16 @@ candidate = []
 votes = []
 race = []
 # skip_rows = []
-start_page = 1
-end_page = 2
+continuation = False
+start_page = 7
+end_page = 7
 while start_page <= end_page:
     data = tabula.read_pdf(pdf_file,multiple_tables=True,lattice=False,stream=False,pages=(start_page))
     new = False
     for x in data:
         df = x
         #since the first page has the candidates in the headers, we need to handle this page differently then the rest.  Here we need to strip out the header data to add to the candidate list
+        print(start_page)
         if start_page == 1:
             df.drop(df.columns[0],axis=1,inplace=True)
             for index, row in df.iterrows():
@@ -39,30 +41,51 @@ while start_page <= end_page:
                 if rows[0] == 'Precinct':
                     skip_rows.append(index)
                     count = 1
-                    candidateList = []
-                    while count < len(df.columns.values):
-                        candidateList.append(rows[count])
-                        count = count + 1
-                    #single case issue, where the names of the candidates are broken across two lines.  This combines the lines in the candidatelist array.
-                    if not pd.notnull(df.iloc[index+1,0]):
-                        skip_rows.append(index+1)
-                        count = 0
-                        while count < len(df.columns.values)-1:
-                            if pd.notnull(df.iloc[index+1,count+1]):
-                                candidateList[count] = candidateList[count]+' '+df.iloc[index+1,count+1]
+                    if continuation:
+                        continuation = False
+                    else:
+                        candidateList = []
+                        while count < len(df.columns.values):
+                            
+                            if str(rows[count]).count('Party') == 2:
+                                candidateList.append('Green Party')
+                                candidateList.append('Natural Law Party')
+                            else:
+                                candidateList.append(rows[count])    
                             count = count + 1
-                if rows[0] == 'Total':
-                    skip_rows.append(index+1)
-                    new = True
+                        #single case issue, where the names of the candidates are broken across two lines.  This combines the lines in the candidatelist array.
+                        if not pd.notnull(df.iloc[index+1,0]):
+                            skip_rows.append(index+1)
+                            count = 0
+                            while count < len(df.columns.values)-1:
+                                if pd.notnull(df.iloc[index+1,count+1]):
+                                    candidateList[count] = candidateList[count]+' '+df.iloc[index+1,count+1]
+                                count = count + 1
+                    # print(candidateList[-1].count('Party'))
+                
                 if index not in skip_rows:
                     for i in range(len(df.columns.values)):
                         if i >= 1:
+                            if i == len(df.columns.values)-1:
+                                # print(df.iloc[index,i])
+                                pass
+                            # if i == len(df.columns.values):
+                            #     print(df.iloc[index,i])
+                            race.append(race_name)
                             votes.append(df.iloc[index,i])
                             candidate.append(candidateList[i-1])
                             precinct.append(df.iloc[index,0])
+                if rows[0] == 'Total':
+                    skip_rows.append(index+1)
+                    race_name = df.iloc[index+1,0]
+                    # print(race_name)
+                    new = True
+            if df.iloc[-1,0] != 'Total':
+                continuation = True
+            # print(candidateList)
+
     start_page = start_page+1
 
-final = {'precinct':precinct,'candidate':candidate,'votes':votes}
-new = pd.DataFrame(final)
-new.to_csv('midland.csv')
-print(new)
+# final = {'precinct':precinct,'race':race,'candidate':candidate,'votes':votes}
+# new = pd.DataFrame(final)
+# new.to_csv('midland.csv')
